c41eb8a2c0bbf57ad67bda99b4ab706c426aaa71
948CSheLL <2418942810@qq.com>
2022-02-27 22:42:59 -0800

    modify lower_bound_danyuan, upper_bound_danyuan and add danyuan

---
+++ .third_part/algorithm_code/danyuan_单元测试框架/c/danyuan.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "CUnit/Basic.h"
+
+extern int function_name(void);
+int judge_expression(void);
+void testFUNCTION_NAME(void);
+
+int main() {
+    CU_pSuite pSuite = NULL;
+
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    pSuite = CU_add_suite("Suite_1", NULL, NULL);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(pSuite, "test of function_name()", testFUNCTION_NAME)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
+
+void testLOWER_BOUND(void) {
+    CU_ASSERT(judge_expression());
+}
