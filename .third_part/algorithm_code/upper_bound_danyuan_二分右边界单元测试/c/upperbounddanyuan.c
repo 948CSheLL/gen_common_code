09d3258c4beafc4050907335cdeb25c2efeb79e1
948CSheLL <2418942810@qq.com>
2022-02-28 21:21:48 -0800

    modify /home/duoyun/.vim/bundle/gen_common_code/.third_part/algorithm_code/upper_bound_danyuan_二分右边界单元测试/c/upperbounddanyuan.c

--- .third_part/algorithm_code/upper_bound_danyuan_二分右边界单元测试/c/upperbounddanyuan.c
+++ .third_part/algorithm_code/upper_bound_danyuan_二分右边界单元测试/c/upperbounddanyuan.c
@@ -1,28 +1,33 @@
-#include <stdio.h>
-#include <string.h>
 #include "CUnit/Basic.h"
 
 extern int *upper_bound(int *, int *, int);
 
 struct TestData {
     int array_list[105];
-    int array_size;
+    int array_l;
+    int array_r;
     int search_val;
     int desired_ans;
+    int is_null;
 } test_data[] = {
-    { {1, 1, 2, 3, 3}, 5, 2, 3},
-    { {1, 1, 3, 4, 5}, 5, 1, 2},
-    { {1, 2, 3, 5, 5}, 5, 5, 5},
-    { {1, 2, 3, 4, 5}, 5, 5, 5},
-    { {1, 4, 5, 5, 8}, 5, 3, 1},
-    { {1, 4, 5, 5, 8}, 5, 6, 4},
-    { {1, 4, 5, 5, 8}, 5, 9, 5},
-    { {1, 4, 6, 6, 8}, 5, 5, 2},
+    { {1, 1, 2, 3, 3}, 0, 0, 2, 3, 1}, 
+    { {1, 1, 2, 3, 3}, 1, 0, 2, 3, 1}, 
+    { {1, 1, 3, 4, 5}, 0, 5, 1, 2, 0}, 
+    { {1, 2, 3, 5, 5}, 0, 5, 5, 5, 0}, 
+    { {1, 2, 3, 4, 5}, 0, 5, 5, 5, 0}, 
+    { {1, 4, 5, 5, 8}, 0, 5, 3, 1, 0}, 
+    { {1, 4, 5, 5, 8}, 0, 5, 6, 4, 0}, 
+    { {1, 4, 5, 5, 8}, 0, 5, 9, 5, 0}, 
+    { {1, 4, 6, 6, 8}, 0, 5, 5, 2, 0}, 
 };
 
 int judge_expression(struct TestData temp) {
-    int ans = upper_bound(temp.array_list, temp.array_list + temp.array_size, temp.search_val) - temp.array_list;
-    return ans == temp.desired_ans;
+    int *p_ans = upper_bound(temp.array_list + temp.array_l, temp.array_list + temp.array_r, temp.search_val);
+    if(temp.is_null) {
+        return NULL == p_ans;
+    } else {
+        return (p_ans - temp.array_list - temp.array_l) == temp.desired_ans;
+    }
 }
 
 void testUPPER_BOUND(void) {
@@ -34,26 +39,28 @@ void testUPPER_BOUND(void) {
     CU_ASSERT(judge_expression(test_data[5]));
     CU_ASSERT(judge_expression(test_data[6]));
     CU_ASSERT(judge_expression(test_data[7]));
+    CU_ASSERT(judge_expression(test_data[8]));
 }
 
-int main() {
-    CU_pSuite pSuite = NULL;
+int main()
+{
+   CU_pSuite pSuite = NULL;
 
-    if (CUE_SUCCESS != CU_initialize_registry())
-        return CU_get_error();
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
 
-    pSuite = CU_add_suite("Suite_1", NULL, NULL);
-    if (NULL == pSuite) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
+   pSuite = CU_add_suite("Suite_1", NULL, NULL);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
 
-    if (NULL == CU_add_test(pSuite, "test of upper_bound()", testUPPER_BOUND)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-    CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
-    CU_cleanup_registry();
-    return CU_get_error();
+   if (NULL == CU_add_test(pSuite, "test of upper_bound()", testUPPER_BOUND)) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
 }
